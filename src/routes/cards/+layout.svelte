27a13a803c0ee346b79ad2c8eb425f19544fe574
eallenop <e>
2024-05-23 16:56:31 +1200

    Add placeholder content to each subpage

---
+++ src/routes/cards/+layout.svelte
@@ -0,0 +1,10 @@
+<section>
+  <slot />
+  <p>Every page in the cards section</p>
+</section>
+
+<style>
+  section {
+    border: 1px dotted blue;
+  }
+</style>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,2 @@
+<h1>View my profile</h1>
+
